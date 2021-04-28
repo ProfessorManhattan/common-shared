791c4de1d2513e472b189cfe8df1746ca34c8fa1
Brian Zalewski <brian@megabyte.space>
2021-04-28 02:07:39 +0000

    Update log.js, update.lib.sh files

--- log.js
+++ log.js
@@ -5,5 +5,9 @@ const info = (message) => consola.info(message);
 const error = (message) => consola.error(message);
 const warn = (message) => consola.warn(message);
 const log = (message) => consola.log(message);
+const fatal = (message) => consola.fatal(message);
+const debug = (message) => consola.debug(message);
+const trace = (message) => consola.trace(message);
+const verbose = (message) => consola.verbose(message);
 
-module.exports = { success, info, error, warn, log };
+module.exports = { success, info, error, warn, log, fatal, debug, trace, verbose };
