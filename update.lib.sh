9ef8447fbb5233f697c70ffbf6e4600958a81be7
Brian Zalewski <brian@megabyte.space>
2021-04-29 22:37:22 +0000

    Update update.lib.sh

--- update.lib.sh
+++ update.lib.sh
@@ -232,7 +232,7 @@ ensure_jq_installed () {
         local DOWNLOAD_SHA256=af986793a515d500ab2d35f8d2aecd656e764504b789b66d7e1a0b727a124c44
         local DOWNLOAD_URL=https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
         local USER_BIN_FOLDER="$HOME/.local/bin"
-        if [ ! -f "$DESTINATION" ] && [ ! command_exists jq ]; then
+        if [ ! -f "$DESTINATION" ] && ! command_exists jq; then
             info "jq is not currently installed"
             log "Ensuring the ~/.local/bin folder exists"
             mkdir -p $USER_BIN_FOLDER
