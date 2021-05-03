9f4a45429c49955220f05488a68e3d59d4be1069
Brian Zalewski <brian@megabyte.space>
2021-05-03 18:43:38 +0000

    Update update.lib.sh

--- update.lib.sh
+++ update.lib.sh
@@ -654,7 +654,7 @@ copy_project_files_and_generate_package_json () {
       # Ensures the scripts.build:slim value matches the value in .blueprint.json
       log "Ensuring the 'build:slim' variable in package.json is updated"
       local DOCKERSLIM_COMMAND=$(cat .blueprint.json | jq '.dockerslim_command' | cut -d '"' -f 2)
-      sed -i .bak 's^DOCKER_SLIM_COMMAND_HERE^'\'"${DOCKERSLIM_COMMAND}"\''^g' package.json && rm package.json.bak
+      sed -i .bak 's^DOCKER_SLIM_COMMAND_HERE^'"${DOCKERSLIM_COMMAND}"'^g' package.json && rm package.json.bak
       success "Successfully ensured that the right 'build:slim' value is included in package.json"
     else
       log "Removing DockerSlim-specific tasks in package.json"
