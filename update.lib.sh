6f3588fb3a4b80e3144541a1ea5f4bbd42ad3b27
Brian Zalewski <brian@megabyte.space>
2021-05-12 23:02:22 +0000

    Update update.lib.sh

--- update.lib.sh
+++ update.lib.sh
@@ -6,9 +6,11 @@
 # functions that perform maintenance tasks and install missing
 # software requirements like Node.js and Python.
 
+
 ##############################################
 ############## COMMON FUNCTIONS ##############
 ##############################################
+
 
 # Logs an error message
 error() {
