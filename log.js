c593ea7404c3baffcabcb3254a5a6002fca2e5c6
Brian Zalewski <brian@megabyte.space>
2021-06-12 15:28:29 -0400

    chore(update) Updating from Code repository update.sh

--- log.js
+++ log.js
@@ -1,17 +1,17 @@
-const consola = require('consola');
+const consola = require('consola')
 
 const logger = consola.create({
-  level: 5
-});
+  level: 5,
+})
 
-const success = (message) => logger.success(message);
-const info = (message) => logger.info(message);
-const error = (message) => logger.error(message);
-const warn = (message) => logger.warn(message);
-const log = (message) => logger.log(message);
-const fatal = (message) => logger.fatal(message);
-const debug = (message) => logger.debug(message);
-const trace = (message) => logger.trace(message);
-const verbose = (message) => logger.verbose(message);
+const success = (message) => logger.success(message)
+const info = (message) => logger.info(message)
+const error = (message) => logger.error(message)
+const warn = (message) => logger.warn(message)
+const log = (message) => logger.log(message)
+const fatal = (message) => logger.fatal(message)
+const debug = (message) => logger.debug(message)
+const trace = (message) => logger.trace(message)
+const verbose = (message) => logger.verbose(message)
 
-module.exports = { success, info, error, warn, log, fatal, debug, trace, verbose };
+module.exports = { success, info, error, warn, log, fatal, debug, trace, verbose }
