092456b32f922b81fa156da31dd92e2b2f0e61de
Brian Zalewski <brian@megabyte.space>
2021-06-21 00:08:22 +0000

    Update lib.sh, common/.yamllint, common/.cspell.json, common/.editorconfig, update.sh, .gitlab-common-ci.yml files Deleted .cspell.json

---
+++ update.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# shellcheck disable=SC1091
+
+# @file .common/update.sh
+# @brief Ensures the project is up-to-date with the latest upstream changes
+# @description
+#   This script performs maintenance on this repository. It includes several bash script
+#   libraries and then it:
+#
+#   1. Ensures Node, jq, and Task are installed
+#   2. Bootstraps the project by using Task to run initialization tasks which bootstrap the project
+#   3. Notifies the user about missing software dependencies that require root priviledges to install
+
+set -e
+
+source "./.common/scripts/log.sh"
+source "./.common/scripts/common.sh"
+source "./.common/scripts/software.sh"
+source "./.common/scripts/notices.sh"
+source "./.common/lib.sh"
+
+if [ "${container:=}" != 'docker' ]; then
+  info "Ensuring Node.js, Task, jq, and yq are installed"
+  ensureNodeSetup &
+  ensureJQInstalled &
+  ensureTaskInstalled &
+  wait
+  success "Node.js, Task, and jq are all installed"
+fi
+
+task requirements update
+
+if [ "${container:=}" != 'docker' ]; then
+  missingDockerNotice
+  missingVirtualBoxNotice
+fi
