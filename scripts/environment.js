3f1f37d5b33106b5a72b440b90c761168b253540
Brian Zalewski <brian@megabyte.space>
2021-06-22 05:58:03 +0000

    Update scripts/environment.js

--- scripts/environment.js
+++ scripts/environment.js
@@ -1,13 +1,11 @@
-'use strict'
+'use strict';
 
-import inquirer from 'inquirer'
-import signale from 'signale'
-import * as fs from 'fs'
+import inquirer from 'inquirer';
+import signale from 'signale';
+import * as fs from 'fs';
 
-signale.info(
-  'Answer the prompt below to switch between environments. Each environment should be a folder with folders and files you wish to link to from the root of the project. They should normally consist of a host_vars, group_vars, inventory, and files folder (but can contain any files/folders you wish to link). Each environment should have its own folder in the `environments/` folder titled as the name of the environment. After you select an answer, the script will symlink all of the items in the environments folder to the root as long as there is not anything except a symlink in the target location (i.e. it will overwrite symlinks but not files).'
-)
-promptForEnv()
+signale.info('Answer the prompt below to switch between environments. Each environment should be a folder with folders and files you wish to link to from the root of the project. They should normally consist of a host_vars, group_vars, inventory, and files folder (but can contain any files/folders you wish to link). Each environment should have its own folder in the `environments/` folder titled as the name of the environment. After you select an answer, the script will symlink all of the items in the environments folder to the root as long as there is not anything except a symlink in the target location (i.e. it will overwrite symlinks but not files).');
+promptForEnv();
 
 /**
  * Prompts the user for the environment they wish to use by asking them
@@ -20,44 +18,36 @@ async function promptForEnv() {
       type: 'list',
       name: 'environment',
       message: 'Which environment would you like to use?',
-      choices: getDirectories('environments/')
+      choices: getDirectories('environments/'),
     }
-  ])
-  const env = response.environment
-  const elements = fs.readdirSync('environments/' + env + '/')
-  let flawless = true
+  ]);
+  const env = response.environment;
+  const elements = fs.readdirSync('environments/' + env + '/');
+  let flawless = true;
   for (const element of elements) {
-    const target = './environments/' + env + '/' + element
+    const target = './environments/' + env + '/' + element;
     if (fs.existsSync(element)) {
       fs.lstat(element, (err, stats) => {
         if (!stats.isSymbolicLink()) {
-          signale.error(
-            'The ' +
-              element +
-              ' target in your project root is not a symbolic link. If you plan on using this feature then you should store any folders/files you wish to be considered part of an environment in the environments/{{ environment_name }}/ folder. You can then use this script to handle creating the symbolic links for you. We are skipping the creation of the symlink to environments/' +
-              env +
-              '/' +
-              element +
-              ' because there is a non-symbolic link with the same name in the root of the project.'
-          )
-          flawless = false
+          signale.error('The `' + element + '` target in your project root is not a symbolic link. If you plan on using this feature then you should store any folders/files you wish to be considered part of an environment in the `environments/{{ environment_name }}/` folder. You can then use this script to handle creating the symbolic links for you. We are skipping the creation of the symlink to `environments/' + env + '/' + element + '` because there is a non-symbolic link with the same name in the root of the project.');
+          flawless = false;
         } else {
-          fs.unlinkSync(element)
+          fs.unlinkSync(element);
           fs.symlinkSync(target, element, (err, stats) => {
-            signale.note(element + ' is now linked to environments/' + env + '/' + element + '.')
-          })
+            signale.note(element + ' is now linked to environments/' + env + '/' + element + '.');
+          });
         }
-      })
+      });
     } else {
       fs.symlinkSync(target, element, (err, stats) => {
-        signale.note(element + '/ is now linked to environments/' + env + '/' + element + '.')
-      })
+        signale.note(element + '/ is now linked to environments/' + env + '/' + element + '.');
+      });
     }
   }
   if (flawless) {
-    signale.success('The ' + env + ' environment is now active.')
+    signale.success('The ' + env + ' environment is now active.');
   } else {
-    signale.warn('There was an error linking the ' + env + ' environment.')
+    signale.warn('There was an error linking the ' + env + ' environment.');
   }
 }
 
@@ -69,6 +59,6 @@ async function promptForEnv() {
  */
 function getDirectories(path) {
   return fs.readdirSync(path).filter(function (file) {
-    return fs.statSync(path + '/' + file).isDirectory()
-  })
+    return fs.statSync(path + '/' + file).isDirectory();
+  });
 }
