1076006237f9e254c206824f145ac3c5fdc8c32b
Brian Zalewski <brian@megabyte.space>
2021-12-03 23:47:02 +0000

    Update common/.gitlab/ci/scripts/update-init.sh

--- common/.gitlab/ci/scripts/update-init.sh
+++ common/.gitlab/ci/scripts/update-init.sh
@@ -49,7 +49,7 @@ fi
 # @description Remove old packages
 TMP="$(mktemp)" && sed 's/.*cz-conventional-changelog.*//' < package.json > "$TMP" && mv "$TMP" package.json
 TMP="$(mktemp)" && sed 's/.*config-conventional.*//' < package.json > "$TMP" && mv "$TMP" package.json
-curl -sS https://gitlab.com/megabyte-labs/common/shared/-/raw/master/package.json > temp.json
+rm temp.json
 TMP="$(mktemp)" && jq 'del(.devDependencies["@mblabs/prettier-config"])' package.json > "$TMP" && mv "$TMP" package.json
 TMP="$(mktemp)" && jq 'del(.devDependencies["@commitlint/config-conventional"])' package.json > "$TMP" && mv "$TMP" package.json
 TMP="$(mktemp)" && jq 'del(.devDependencies["@mblabs/eslint-config"])' package.json > "$TMP" && mv "$TMP" package.json
@@ -69,8 +69,8 @@ TMP="$(mktemp)" && jq '.private = false' package.json > "$TMP" && mv "$TMP" pack
 TMP="$(mktemp)" && jq '.devDependencies["@washingtondc/development"] = "^1.0.2"' package.json > "$TMP" && mv "$TMP" package.json
 TMP="$(mktemp)" && jq '.devDependencies["@washingtondc/prettier"] = "^1.0.0"' package.json > "$TMP" && mv "$TMP" package.json
 TMP="$(mktemp)" && jq '.devDependencies["@washingtondc/release"] = "^0.0.2"' package.json > "$TMP" && mv "$TMP" package.json
-TMP="$(mktemp)" && jq '.devDependencies["eslint-config-strict-mode" = "^1.0.0"]' package.json > "$TMP" && mv "$TMP" package.json
-TMP="$(mktemp)" && jq '.devDependencies["sleekfast" = "^0.0.1"]' package.json > "$TMP" && mv "$TMP" package.json
+TMP="$(mktemp)" && jq '.devDependencies["eslint-config-strict-mode"] = "^1.0.0"' package.json > "$TMP" && mv "$TMP" package.json
+TMP="$(mktemp)" && jq '.devDependencies["sleekfast"] = "^0.0.1"' package.json > "$TMP" && mv "$TMP" package.json
 
 # @description Re-generate the Taskfile.yml if it has invalid includes
 echo "Ensuring Taskfile is properly configured"
