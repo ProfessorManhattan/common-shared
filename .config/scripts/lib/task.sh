c9b6c5620cf18f9e40c7b1fb775ba0824174b36a
Megabyte Labs Automation <help@megabyte.space>
2021-12-29 17:40:18 +0000

    ⤵️ automation(synchronize) Applying changes from upstream repository.

--- .config/scripts/lib/task.sh
+++ .config/scripts/lib/task.sh
@@ -49,8 +49,7 @@ function ensureLocalPath() {
     # shellcheck disable=SC2016
     local PATH_STRING='\nexport PATH=$HOME/.local/bin:$PATH'
     if grep -L "$PATH_STRING" "$SHELL_PROFILE"; then
-      echo -e "export ${PATH_STRING}\n" >> "$SHELL_PROFILE"
-      echo "$SHELL_PROFILE" > "$TMP_PROFILE_PATH"
+      echo -e "$PATH_STRING" >> "$SHELL_PROFILE"
       .config/log info "Updated the PATH variable to include ~/.local/bin in $SHELL_PROFILE"
     fi
   elif [[ "$OSTYPE" == 'cygwin' ]] || [[ "$OSTYPE" == 'msys' ]] || [[ "$OSTYPE" == 'win32' ]]; then
