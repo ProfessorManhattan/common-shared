1c477d4ee218fbdf1188b01e018b789bdfc172f5
Brian Zalewski <brian@megabyte.space>
2022-09-27 07:40:36 +0000

    Update common/start.sh

--- common/start.sh
+++ common/start.sh
@@ -527,6 +527,17 @@ if [ ! -f "$HOME/.profile" ]; then
   touch "$HOME/.profile"
 fi
 
+# @description Ensure git hosts are all in ~/.ssh/known_hosts
+if [ ! -n "$(grep "^gitlab.com " ~/.ssh/known_hosts)" ]; then
+  ssh-keyscan gitlab.com >> ~/.ssh/known_hosts 2>/dev/null
+fi
+if [ ! -n "$(grep "^github.com " ~/.ssh/known_hosts)" ]; then
+  ssh-keyscan github.com >> ~/.ssh/known_hosts 2>/dev/null
+fi
+if [ ! -n "$(grep "^bitbucket.org " ~/.ssh/known_hosts)" ]; then
+  ssh-keyscan bitbucket.org >> ~/.ssh/known_hosts 2>/dev/null
+fi
+
 # @description Ensures ~/.local/bin is in PATH
 ensureLocalPath
 
